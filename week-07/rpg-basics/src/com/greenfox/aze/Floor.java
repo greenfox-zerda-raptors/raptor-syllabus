ac2f993dc55d77d98aeff15ad8612b3e51f0c504
Adam Gyulavari <gyulavari.adam@gmail.com>
2016-12-07 09:19:36 +0100

    add wanderer practice

---
+++ week-07/rpg-basics/src/com/greenfox/aze/Floor.java
@@ -0,0 +1,10 @@
+package com.greenfox.aze;
+
+/**
+ * Created by aze on 2016.12.05..
+ */
+public class Floor extends GameObject {
+    public Floor(int posX, int posY) {
+        super("floor.png", posX, posY);
+    }
+}
